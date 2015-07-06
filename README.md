aeb732d71aa5f0199fa7e33c76e3fb0b169eb09e
Anam Ahmed <anam.ahmed.a@gmail.com>
2015-07-06 14:38:05 +0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,21 @@
+##Evil Evaluator
+
+Live demo: <theanam.github.io/evileval>
+====
+
+A small and **highly experimental** project for live code demonstration in JavaScript during presentations or public speeches.
+
+###How to use
+
+Write code in the top part, the bottom part acts like the console output. every `console.log` will output there.
+
+To run the code press the run button or press <kbd>Ctrl</kbd>+<kbd>K</kbd>
+
+###Contributing
+
+Any good patches are always appreiated
+
+
+### Tools Used
+
+Code mirror - <http://codemirror.net>
---
+++ script.js
@@ -0,0 +1,28 @@
+var repl = document.querySelector('.repl');
+var fakeConsole = {
+    log:function(output){
+        var line = document.createElement('li');
+        line.innerHTML = output;
+        document.querySelector('.result .lines').appendChild(line);
+    }
+};
+var replManager = CodeMirror(repl,{
+    mode:'javascript',
+    theme:'monokai',
+    lineNumbers:true
+});
+
+document.querySelector('.action').addEventListener('click',evaluate);
+document.addEventListener('keyup',function(e){
+    if(e.ctrlKey && e.key=='e'){
+        evaluate();
+    }
+})
+function evaluate(){
+    //clear output if any
+    document.querySelector('.result .lines').innerHTML="";
+    //load the contents as a function
+   var expressions = replManager.getValue();
+    var action = new Function('console',expressions);
+    action(fakeConsole);
+};
