6a63b30425366a2b8b0c5f6a7a2423d872ba0c28
anam.ahmed <anam.ahmed.a@gmail.com>
2018-10-06 02:40:40 +0600

    Updates

--- precache-manifest.d2943771194095b2e4beb3fc9a4eb483.js
+++ precache-manifest.d2943771194095b2e4beb3fc9a4eb483.js
@@ -12,19 +12,19 @@ self.__precacheManifest = [
     "url": "/evileval/static/js/runtime~main.303c555d.js"
   },
   {
-    "revision": "0ad5be7d42587ba26384",
-    "url": "/evileval/static/js/main.0ad5be7d.chunk.js"
+    "revision": "b0c637b10b45ab84cee7",
+    "url": "/evileval/static/js/main.b0c637b1.chunk.js"
   },
   {
     "revision": "7aaa5b7bbf7568d14cb7",
     "url": "/evileval/static/js/1.7aaa5b7b.chunk.js"
   },
   {
-    "revision": "0ad5be7d42587ba26384",
+    "revision": "b0c637b10b45ab84cee7",
     "url": "/evileval/static/css/main.87736f7d.chunk.css"
   },
   {
-    "revision": "7f22f36165159f8a14ac4e19c6dedd7b",
+    "revision": "6c2e7a99b44f3c216bd500d6b10460bb",
     "url": "/evileval/index.html"
   }
 ];
