26f793951e0c99c0761d9248a6646792e71ed1a1
anam.ahmed <anam.ahmed.a@gmail.com>
2018-10-06 02:40:16 +0600

    header bugfix

---
+++ build/precache-manifest.d2943771194095b2e4beb3fc9a4eb483.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "428753370fb2ec6574894a2057b620af",
+    "url": "/evileval/static/media/share-alt.42875337.svg"
+  },
+  {
+    "revision": "8e4808f106449d40c256bd54732b042d",
+    "url": "/evileval/static/media/gh.8e4808f1.svg"
+  },
+  {
+    "revision": "303c555d89b861b6a14c",
+    "url": "/evileval/static/js/runtime~main.303c555d.js"
+  },
+  {
+    "revision": "b0c637b10b45ab84cee7",
+    "url": "/evileval/static/js/main.b0c637b1.chunk.js"
+  },
+  {
+    "revision": "7aaa5b7bbf7568d14cb7",
+    "url": "/evileval/static/js/1.7aaa5b7b.chunk.js"
+  },
+  {
+    "revision": "b0c637b10b45ab84cee7",
+    "url": "/evileval/static/css/main.87736f7d.chunk.css"
+  },
+  {
+    "revision": "6c2e7a99b44f3c216bd500d6b10460bb",
+    "url": "/evileval/index.html"
+  }
+];
