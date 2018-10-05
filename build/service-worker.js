6af04b6a9f01ca4aa1ade80c2d442d5464637f16
anam.ahmed <anam.ahmed.a@gmail.com>
2018-10-06 01:45:01 +0600

    build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/evileval/precache-manifest.95db58b81e551de0ff2b56b7b93ae7d8.js"
+  "/evileval/precache-manifest.4e3d530abcafded784cf45804b311700.js"
 );
 
 workbox.clientsClaim();
